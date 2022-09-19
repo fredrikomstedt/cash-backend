4631c57ec0bb06977b495eaed89a707abe61d9e9
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2022-09-19 10:00:27 +0200

    Removed interfaces due to problems with circular imports and dependencies
    This would be solved by having a Bind functionality in the dependency injection framework, but alas we must code in a subpar fashion

--- src/authentication/authentication.py
+++ src/authentication/authentication.py
@@ -1,20 +1,14 @@
-from abc import ABC, abstractmethod
-
-
-class IAuthentication(ABC):
-    @abstractmethod
+class Authentication:
     def create_user(self, username: str, password: str) -> None:
         """Creates a user with the supplied username and password,
            assuming no user with that username already exists."""
 
-    @abstractmethod
     def login_user(self, username: str, password: str) -> str:
         """Logs in a user with the supplied username and password,
            assuming the user exists and is verified.
 
            Returns a token for authenticated requests."""
 
-    @abstractmethod
     def authenticate_user(self, token: str):  # -> User
         """Authenticates the supplied token and returns
            the user associated with it."""
--- src/authentication/password_handler.py
+++ src/authentication/password_handler.py
@@ -2,15 +2,16 @@ from fastapi import Depends
 from passlib.context import CryptContext
 
 from .dependencies import get_crypt_context
-from .i_password_handler import IPasswordHandler
 
 
-class PasswordHandler(IPasswordHandler):
+class PasswordHandler:
     def __init__(self, crypt_context: CryptContext = Depends(get_crypt_context)):
         self.__context = crypt_context
 
     def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        """Verifies that the plain_password matches the hashed_password."""
         return self.__context.verify(plain_password, hashed_password)
 
     def hash_password(self, plain_password: str) -> str:
+        """Hashes the supplied password and returns it."""
         return self.__context.hash(plain_password)
